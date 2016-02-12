5271b7b26ff1eb65d1b952a56701a093ca00500b
Connor Spencer Harries <connor@smirking.ninja>
2016-02-12 14:22:04 +0000

    Add Apache License. Delete dummy module.

--- bungee-rebel-plugin/src/main/java/ninja/smirking/rebel/BungeeReloader.java
+++ bungee-rebel-plugin/src/main/java/ninja/smirking/rebel/BungeeReloader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Connor Spencer Harries
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ninja.smirking.rebel;
 
 import java.util.logging.Level;
@@ -7,6 +23,8 @@ import net.md_5.bungee.api.plugin.Plugin;
 import org.zeroturnaround.javarebel.ClassEventListener;
 
 /**
+ * Attempts to disable and re-enable a {@link Plugin} when one of it's classes are reloaded.
+ *
  * @author Connor Spencer Harries
  */
 public enum BungeeReloader implements ClassEventListener {
